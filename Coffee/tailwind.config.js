3e38d89b996c87f0927d34e81cabf3c0767bf441
Dzu_203 <108659135+Dzu-203@users.noreply.github.com>
2024-04-12 12:11:18 +0700

    complete cf
    complete cf

---
+++ Coffee/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors :{
+        'coffee' : "#B87747"
+      }
+    },
+  },
+  plugins: [],
+}
+
