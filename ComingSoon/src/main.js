836edacd568f23e9c0b888cd86a29167a4d80eac
Dzu_203 <108659135+Dzu-203@users.noreply.github.com>
2024-04-18 16:12:17 +0700

    update - sửa tên file

--- ComingSoon/src/main.js
+++ ComingSoon/src/main.js
@@ -1,34 +1,46 @@
-const days = document.querySelector('.days')
-const hours = document.querySelector('.hours')
-const minutes = document.querySelector('.minutes')
-const seconds = document.querySelector('.seconds')
-const number = document.querySelectorAll('.number')
+const days = document.querySelector('.days');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
 
-let day = 9
-let hour = 30
-let minute = 25
-let second = 10
+const time = {
+  day: 9,
+  hour: 30,
+  minute: 25,
+  second: 10,
 
+  updateDisplay: function() {
+    days.innerHTML = this.formatTime(this.day);
+    hours.innerHTML = this.formatTime(this.hour);
+    minutes.innerHTML = this.formatTime(this.minute);
+    seconds.innerHTML = this.formatTime(this.second);
+  },
+
+  formatTime: function(timeValue) {
+    return timeValue < 10 ? `0${timeValue}` : timeValue;
+  },
+
+  tick: function() {
+    this.second--;
+    if (this.second === 0) {
+      this.second = 60;
+      this.minute--;
+    }
+    if (this.minute === 0) {
+      this.minute = 60;
+      this.hour--;
+    }
+    if (this.hour === 0) {
+      this.hour = 60;
+      this.day--;
+    }
+    if (this.day === 0) {
+      clearInterval(timeming);
+    }
+    this.updateDisplay();
+  }
+};
 
 let timeming = setInterval(() => {
-    second--
-    if (second === 0){
-        second = 60
-        minute--
-    }
-    if (minute === 0){
-        minute = 60
-        hour--
-    }
-    if (hour === 0){
-        hour = 60
-        day--
-    }
-    if (day === 0){
-        clearInterval()
-    }
-    days.innerHTML = day < 10 ? `0${day}` : day
-    hours.innerHTML = hour < 10 ? `0${hour}` : hour
-    minutes.innerHTML = minute < 10 ? `0${minute}` : minute
-    seconds.innerHTML = second < 10 ? `0${second}` : second
-},1000)
+  time.tick();
+}, 1000);
